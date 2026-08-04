1e91fa2b381f5566b76004bf3c374cf58100c70d
Waqas Zulfiqar <waqaszulfiqar715@gmail.com>
2026-08-04 12:01:27 +0300

    Optimize institutional session window: Set Peak London & NY Session (07:00 UTC - 16:00 UTC)

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ DIVERGENCE_LOOKBACK = 10 # 5m candles to look back for RSI divergence
 TIMEFRAME = "5m"  # 5 minutes
 
 # Session Config (UTC hours)
-# Active Institutional Forex Window: London & New York Sessions (07:00 to 19:00 UTC)
-# Avoids late US afternoon / Asian Rollover dead zone (19:00 to 07:00 UTC) where liquidity is low.
+# Active Institutional Forex Window: Peak London & New York Sessions (07:00 to 16:00 UTC)
+# Avoids late US afternoon / Asian Rollover dead zone (16:00 to 07:00 UTC) where liquidity is low.
 SESSION_START_UTC = 7
-SESSION_END_UTC = 19
+SESSION_END_UTC = 16
